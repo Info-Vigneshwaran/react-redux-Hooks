161af7cf8ff34fa0ae6bfb423f4b0663a851c367
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-29 17:57:38 +0530

    1 file modified, 2 deleted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import rootReducer from './reducers/rootReducer';
+import { createStore } from 'redux';
 
-import App from "./App";
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+ReactDOM.render(<App />, document.getElementById('root'));
